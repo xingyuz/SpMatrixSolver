c254e6992ae9e75f270ddbe0ec761d0c408c157e
xingyuz <xingyuz@mtu.edu>
2018-04-18 01:19:28 -0400

    tst

--- test.m
+++ test.m
@@ -1,16 +1,12 @@
 clear all;
-A = mmread('fe_4elt2.mtx');  % read adjacency matrix of graph
+G = mmread('fe_4elt2.mtx');  % read adjacency matrix of graph
 % A = mmread('fe_ocean.mtx');
 % A = mmread('fe_rotor.mtx');
 % A = mmread('fe_sphere.mtx');
 % A = mmread('fe_tooth.mtx');
-G=diag(sum(A))-A;
-tic
-[L,U,P] = lu(G);
-toc
-tic
-LL = chol(G,'lower');
-toc
-tic
-RR = chol(G,'upper');
-toc
+A=diag(sum(G))-G;
+I=speye(length(A));
+A=A+1e-1*I;
+x=sparse(rand(length(A),1)*1);
+b=A*x;
+[x itr] = jacobi(A,b);
