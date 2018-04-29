a33eae574aa2ac2a3049a5589f90a5c656d8662f
xingyuz <xingyuz@mtu.edu>
2018-04-29 03:40:35 -0400

    update

--- convplot.m
+++ convplot.m
@@ -3,13 +3,14 @@ figure ( 1 )
 set(gca,'FontSize', 18);
 set(gca, 'YScale', 'log')
 hold
-plot(1:iter1, (conv1));
-plot(1:iter2, (conv2));
 plot(1:iter3, (conv3));
 plot(1:iter4, (conv4));
 plot(1:iter5, (conv5));
+plot(1:iter6, (conv6));
+plot(1:iter7, (conv7));
+plot(1:iter8, (conv8));
 legend('Jacobi','Gauss-seidel','SD','CG','PCG-ichol','PCG-SS');
-title ( 'fe\_4elt2 Iterative Methods Convergence Bahavior' )
+title ( [filename, ' Iterative Methods Convergence Bahavior'])
 xlabel ( 'Iterations' )
 ylabel ( 'Norms of Residual' )
 grid
--- test.m
+++ test.m
@@ -1,11 +1,11 @@
 clear all;
 rng default;
-G = mmread('fe_4elt2.mtx');  % read adjacency matrix of graph
-% G = mmread('fe_ocean.mtx');
-% G = mmread('fe_rotor.mtx');
-% G = mmread('fe_sphere.mtx');
-% G = mmread('fe_tooth.mtx');
-%
+filename = 'fe_4elt2.mtx'; 
+% filename = 'fe_ocean.mtx'; 
+% filename = 'fe_rotor.mtx'; 
+% filename = 'fe_sphere.mtx'; 
+% filename = 'fe_tooth.mtx'; 
+G = mmread(filename);  % read adjacency matrix of graph
 A=diag(sum(G))-G;
 I=speye(length(A));
 A=A+1e-6*I;
@@ -14,18 +14,23 @@ b=A*x;
 x0=b;
 tol=1e-3;
 tic;xx=A\b;toc
-% tic;[x1] = LUsolver(A,b);t1=toc;
-% tic;[x2] = cholsolver(A,b);t2=toc;
-% tic;[x3,iter3,conv3] = jacobi(A,b,x0,tol);t3=toc;
-% tic;[x4,iter4,conv4] = gauss_seidel(A,b,x0,tol);t4=toc;
-% tic;[x5,iter5,conv5] = steepestdescent(A,b,x0,tol);t5=toc;
-% tic;[x6,iter6,conv6] = conj_gradient(A,b,x0,tol);t4=toc;
-% L1=ichol(A); % Incomplete cholesky factorized preconditioner
-% M1=L1*L1';
-% tic;[x7,iter7,conv7] = mypcg2(A,b,x0,L1,tol);t6=toc;
-% L2 = ssmatrix('Pmat.mtx');
-% M2 = L2*L2';
-% tic;[x8,iter8,conv8] = mypcg2(A,b,x0,L2,tol);t6=toc;
+tic;[x1] = LUsolver(A,b);t1=toc;
+tic;[x2] = cholsolver(A,b);t2=toc;
+tic;[x3,iter3,conv3] = jacobi(A,b,x0,tol);t3=toc;
+tic;[x4,iter4,conv4] = gauss_seidel(A,b,x0,tol);t4=toc;
+tic;[x5,iter5,conv5] = steepestdescent(A,b,x0,tol);t5=toc;
+tic;[x6,iter6,conv6] = conj_gradient(A,b,x0,tol);t6=toc;
+% Incomplete cholesky factorized preconditioner
+L1=ichol(A); 
+M1=L1*L1';
+tic;[x7,iter7,conv7] = mypcg2(A,b,x0,L1,tol);t7=toc;
+% Spectral sparsification preconditioner
+L2 = ssmatrix(filename);  
+M2 = L2*L2';
+tic;[x8,iter8,conv8] = mypcg2(A,b,x0,L2,tol);t8=toc;
+
+%plot convergence behavior
+convplot;
 
 % tic;[x1,fl1,rr1,it1,rv1] = pcg(A,b,1e-5,1000,M);t10=toc;
 % tic;[x2,f22,rr2,it2,rv2] = pcg(A,b,1e-5,1000,L,L');t11=toc;
